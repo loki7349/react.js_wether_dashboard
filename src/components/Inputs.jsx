90d150abde41ff4559040fa7da857171647bc61a
Lokesh Babchode <108623364+loki7349@users.noreply.github.com>
2024-02-02 14:14:43 +0000

    Added the Top  buttons

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { UilSearch, UilMapMarker } from '@iconscout/react-unicons'
+
+function Inputs(){
+    return (
+        <div className="flex "></div>
+    )
+}
+
+export default Inputs;
